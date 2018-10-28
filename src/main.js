f2dd05e7b5ca74d47943110d2e060c69ff25391c
mazarag2 <mazarag2@asu.edu>
2018-10-28 16:35:03 -0700

    move to main.js

--- src/main.js
+++ src/main.js
@@ -11,5 +11,32 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  methods:{
+	  
+	  onSignIn : function(googleUser){
+	
+	  var authToken = googleUser.getAuthResponse().id_token;
+	 
+	  console.log('Name: ' + profile.getName());
+	  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	  
+	  var name = profile.getName();
+	  var email = profile.getEmail();
+	  
+	  var url = "http://localhost:8081/index";
+	  
+	  axios.post(url,{
+			token : authToken,
+			Email : email
+		}
+	  ).then(response => {
+		console.log(response);
+	  }).catch(e => {
+		console.log("error", e);
+	  });			
+
+	}
+	  
+  }
 })
